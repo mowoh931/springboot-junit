fbc970e8051caee350429073a95690a2122ae589
JayMgitM <mowohbaar@yahoo.com>
2023-12-10 13:38:13 -0600

    added entity class

---
+++ src/main/java/com/baar/springbootjunit/model/Person.java
@@ -0,0 +1,19 @@
+package com.baar.springbootjunit.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table
+@Entity
+public class Person {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  int id;
+  String name;
+  String city;
+}
